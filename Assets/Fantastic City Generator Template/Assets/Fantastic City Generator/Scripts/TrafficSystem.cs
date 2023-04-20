d79e5fa19dff6685b735ada1efdb6892f60d2e4e
J.D. DeVaughn-Brown <jddevaughnbrown@sierracanyonschool.org>
2023-04-20 06:46:44 -0700

    feat: Add Fantastic City Generator Template

---
+++ Assets/Fantastic City Generator Template/Assets/Fantastic City Generator/Scripts/TrafficSystem.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class TrafficSystem : MonoBehaviour {
+
+
+    public GameObject[] IaCars;
+
+    void Awake()
+    {
+        if (GameObject.Find("RoadMark") && GameObject.Find("RoadMarkRev"))
+            InverseCarDirection(true);
+
+        LoadCars();
+    }
+
+    private void InverseCarDirection(bool actualside)
+    {
+
+
+        GameObject[] roadMark = GameObject.FindObjectsOfType(typeof(GameObject)).Select(g => g as GameObject).Where(g => g.name.Equals("Road-Mark")).ToArray();
+        for (int i = 0; i < roadMark.Length; i++)
+            roadMark[i].transform.Find("RoadMark").gameObject.SetActive(actualside);
+
+        roadMark = GameObject.FindObjectsOfType(typeof(GameObject)).Select(g => g as GameObject).Where(g => g.name.Equals("Road-Mark-Rev")).ToArray();
+        for (int i = 0; i < roadMark.Length; i++)
+            roadMark[i].transform.Find("RoadMarkRev").gameObject.SetActive(!actualside);
+
+
+    }
+
+    public void LoadCars()
+    {
+
+        
+        int nVehicles = 0;
+
+        DestroyImmediate(GameObject.Find("CarContainer"));
+
+        Transform CarContainer = new GameObject("CarContainer").transform;
+        
+        GameObject carro;
+
+        GameObject[] tf01 = GameObject.FindObjectsOfType(typeof(GameObject)).Select(g => g as GameObject).Where(g => g.name.Equals("TF-01")).ToArray();
+
+        int n = tf01.Length;
+        for (int i = 0; i < n; i++)
+        {
+
+            carro = (GameObject)Instantiate(IaCars[Mathf.Clamp(Random.Range(0, IaCars.Length), 0, IaCars.Length - 1)], tf01[i].transform.position, tf01[i].transform.rotation);
+            carro.transform.SetParent(CarContainer);
+
+            carro.GetComponent<TrafficCar>().path = tf01[i].transform.parent.gameObject;
+
+            nVehicles++;
+
+        }
+
+        Debug.Log(nVehicles + " vehicles were instantiated");
+
+    }
+
+
+}
---
+++ Assets/Fantastic City Generator Template/Assets/Fantastic City Generator/WayTool/FCGWaypointsContainer.cs
@@ -0,0 +1,165 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class FCGWaypointsContainer : MonoBehaviour {
+
+    //[HideInInspector]        
+    public List<Transform> waypoints = new List<Transform>();
+
+    [HideInInspector]
+    public GameObject[] nextWay;
+
+    private GameObject[] tf01;
+    
+    private bool inLeft = false;
+    
+    private void Awake()
+    {
+       
+        inLeft = GameObject.Find("RoadMarkRev");
+        
+        tf01 = GameObject.FindObjectsOfType(typeof(GameObject)).Select(g => g as GameObject).Where(g => g.name.Equals("TF-01")).ToArray();
+
+            
+        for (int i = 0; i < waypoints.Count; i++)
+        if (i < waypoints.Count - 1)
+            waypoints[i].LookAt(waypoints[i + 1]);
+        else
+        {
+            waypoints[i].rotation = Quaternion.LookRotation(waypoints[i].position - waypoints[i - 1].position);
+            NextWays(waypoints[i]);
+        }
+       
+
+       
+    }
+
+  
+    public void InvertNodesDirection()
+    {
+        Vector3 wtemp = new Vector3(0,0,0);
+         
+        int nn = Mathf.CeilToInt(waypoints.Count / 2);
+   
+        for (int i = 0; i < nn; i++)
+        {
+            wtemp = waypoints[i].position;
+            waypoints[i].position = waypoints[waypoints.Count - i - 1].position;
+            waypoints[waypoints.Count - i - 1].position = wtemp;
+        }
+
+    }
+ 
+
+    private void NextWays(Transform referencia)
+    {
+   
+             
+        int n = tf01.Length;
+
+        if (n < 1) return;
+       
+        ArrayList arr = new ArrayList();
+
+        for (int i = 0; i < n; i++)
+        {
+
+            float distancia = Vector3.Distance(referencia.position, tf01[i].transform.position);
+
+                
+            if (distancia < 35f && distancia > 8f)
+            {
+
+                float a = GetAngulo(referencia, tf01[i].transform);
+
+                if (!inLeft && (a > 340 || a < 80) || inLeft && (a > 280 || a < 20)) 
+                arr.Add(tf01[i].transform);
+                
+            }
+
+        }
+
+        referencia.transform.parent.gameObject.GetComponent<FCGWaypointsContainer>().AddNewWays(referencia, arr);
+
+    }
+
+
+    private void AddNewWays(Transform referencia, ArrayList arr)
+    {
+        Transform stemp;
+
+        nextWay = new GameObject[arr.Count];
+
+        for (int i = 0; i < arr.Count; i++)
+        {
+
+                stemp = (Transform)arr[i];
+                nextWay[i] = stemp.parent.gameObject;
+            
+
+        }
+
+
+
+    }
+
+    private float GetAngulo(Transform origem , Transform target)
+    {
+        float r;
+
+        GameObject compass = new GameObject("Compass");
+        compass.transform.parent = origem;
+        compass.transform.localPosition = new Vector3(0, 0, 0);
+
+        compass.transform.LookAt(target);
+        r = compass.transform.localEulerAngles.y;
+
+        Destroy(compass);
+        return r;
+
+    }
+
+    void OnDrawGizmos() {
+
+        if (waypoints.Count < 1) return;
+
+        for (int i = 0; i < waypoints.Count; i ++){
+
+            Gizmos.color = new Color(0.0f, 1.0f, 0.0f, 0.3f);
+
+            if (waypoints.Count < 1) return;
+
+            Gizmos.DrawSphere(waypoints[i].transform.position, 1f);
+			Gizmos.DrawWireSphere (waypoints[i].transform.position, 2f);
+
+            if (waypoints.Count < 2) return;
+
+            if (i < waypoints.Count - 1){
+				if(waypoints[i] && waypoints[i+1]){
+                    
+					if (waypoints.Count > 0) {
+
+                        if (i < waypoints.Count - 1)
+                        {
+                            Gizmos.DrawLine(waypoints[i].position, waypoints[i + 1].position);
+                            waypoints[i].LookAt(waypoints[i + 1]);
+                           
+                        } 
+
+					}
+				}
+			}
+            else if (i == waypoints.Count - 1)
+            {
+                waypoints[i].rotation = waypoints[i - 1].rotation; 
+            }
+
+        }
+		
+	}
+ 
+    
+}
