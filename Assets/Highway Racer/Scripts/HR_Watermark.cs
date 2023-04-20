0e8f12f396638333d69a32a25da8325a58ef896e
J.D. DeVaughn-Brown <jddevaughnbrown@sierracanyonschool.org>
2023-04-20 06:02:49 -0700

    Initial Commit

---
+++ Assets/Highway Racer/Scripts/HR_Watermark.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HR_Watermark : MonoBehaviour {
+
+	static HR_Watermark instance;
+
+	void Start () {
+
+		if (instance)
+		{
+			Destroy (gameObject);
+		}
+		else
+		{
+			instance = this;
+			DontDestroyOnLoad (gameObject);
+		}
+	}
+
+}
